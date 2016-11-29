36cfdfe44a3257b04a6327973d0ed80ec8aa06d4
Rohitash Tulyani <rtulyani@sapient.com>
2016-11-30 04:21:28 +0530

    Green - Added GetSumOfFactors method implementation

--- src/com/cleancode/perfectnumber/PerfectNumber.java
+++ src/com/cleancode/perfectnumber/PerfectNumber.java
@@ -16,4 +16,14 @@ public class PerfectNumber{
         return false;
     }
 
+    public int getSumOfFactors(int number) {
+        int sum = 0;
+        for (int counter = 1; counter <= number / 2; counter++) {
+            if (number % counter == 0) {
+                sum += counter;
+            }
+        }
+        return sum;
+    }
+
 }
