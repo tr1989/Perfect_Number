a822bd482e941453cecf44868850edc2c8270b6b
Rohitash Tulyani <rtulyani@sapient.com>
2016-11-30 03:36:38 +0530

    Added first test case to check number is postive

--- test/com/cleancode/perfectnumber/PerfectNumberTest.java
+++ test/com/cleancode/perfectnumber/PerfectNumberTest.java
@@ -4,5 +4,11 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class PerfectNumberTest {
-
+    @Test
+    public void testIsNumberPositive(){
+        PerfectNumber perfectNumber = new PerfectNumber();
+        int number = 5;
+        boolean expected = perfectNumber.isNumberPositive(number);
+        Assert.assertTrue(expected);
+    }
 }
