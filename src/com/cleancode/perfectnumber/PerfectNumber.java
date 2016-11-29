364f6d00ff1b063d7f7fec32c0c10fae255e5ad6
Rohitash Tulyani <rtulyani@sapient.com>
2016-11-30 04:02:06 +0530

    use assertFalse for faiure case

--- src/com/cleancode/perfectnumber/PerfectNumber.java
+++ src/com/cleancode/perfectnumber/PerfectNumber.java
@@ -3,6 +3,9 @@ package com.cleancode.perfectnumber;
 public class PerfectNumber{
 
     public  boolean isNumberPositive(int number){
+        if(number > 0){
+            return true;
+        }
         return false;
     }
 
